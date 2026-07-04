7bd3761ae4d83587e5c73a97a29b79c6345c55c5
maiarasoriano <maiarahw.4970@gmail.com>
2026-07-04 12:12:31 +0000

    php-exemplos-basicos

---
+++ inicio-revisao/3_operadores.php
@@ -0,0 +1,14 @@
+<?php
+
+// Declaração das variáveis
+$idade = 15;
+$temDocumento = true;
+
+// Condicional com operador (E)
+if($idade >= 18 && $temDocumento) {
+    echo "Pode tirar a carteira";
+} else {
+    echo "Não pode tirar a carteira";
+}
+
+?>
